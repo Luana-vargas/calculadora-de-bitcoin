b98d26adac18114959f68ced16545a88240f05a7
luana-vargas <vargas.luana@outlook.com.br>
2020-03-12 12:04:56 -0300

    Agrupados layouts na main

--- app/src/main/java/br/com/luana/calculadoradebitcoin/MainActivity.kt
+++ app/src/main/java/br/com/luana/calculadoradebitcoin/MainActivity.kt
@@ -2,11 +2,32 @@ package br.com.luana.calculadoradebitcoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.doAsync
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    val API_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
+
+    var cotacaoBitcoin:Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buscarCotacao()
+
+    }
+
+    fun buscarCotacao() {
+
+        //doAsync, função da lib Anko
+        doAsync {
+
+            //Acessar a API e buscar seu resultado
+            val resposta = URL(API_URL).readText()
+
+        }
+
     }
 }
